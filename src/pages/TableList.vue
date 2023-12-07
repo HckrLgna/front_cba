a82ad9ad8a19893d53890c4fdfbf406381faab1c
Angel Eugenio Charca Blanco <ablanco837@gmail.com>
2023-12-07 13:36:31 -0400

    ignoaramos git ignore

---
+++ src/pages/TableList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <card class="strpied-tabled-with-hover"
+                body-classes="table-full-width table-responsive"
+          >
+            <template slot="header">
+              <h4 class="card-title"> Question bank </h4>
+              <p class="card-category">Table about total question bank </p>
+            </template>
+            <l-table class="table-hover table-striped"
+                     :columns="table1.columns"
+                     :data="table1.data">
+            </l-table>
+          </card>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import LTable from 'src/components/Table.vue'
+
+  const tableColumns = ['Id', 'Topic', 'questions', 'Options']
+  const tableData = [{
+    id: 1,
+    topic: 'Dakota Rice',
+    questions: '4',
+    options: 'edit',
+  },
+  {
+    id: 2,
+    topic: 'Minerva Hooper',
+    questions: '6',
+    options: 'edit',
+  } ]
+  export default {
+    components: {
+      LTable,
+
+    },
+    data () {
+      return {
+        table1: {
+          columns: [...tableColumns],
+          data: [...tableData]
+        }
+      }
+    }
+  }
+</script>
+<style>
+</style>
---
+++ src/pages/UserProfile.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <card class="strpied-tabled-with-hover"
+                body-classes="table-full-width table-responsive"
+          >
+            <template slot="header">
+              <h4 class="card-title">Total Students</h4>
+              <p class="card-category">Table about the number total students </p>
+            </template>
+            <l-table class="table-hover table-striped"
+                     :columns="table1.columns"
+                     :data="table1.data">
+            </l-table>
+          </card>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import LTable from 'src/components/Table.vue'
+  const tableColumns = ['Id', 'Name', 'Email', 'Telefono', 'Options']
+  const tableData = [{
+    id: 1,
+    name: 'Dakota Rice',
+    email: 'dakota@corre.com',
+    telefono: '447512',
+    options: 'edit'
+  },
+  {
+    id: 2,
+    name: 'Minerva Hooper',
+    email: 'minerva@correo.com',
+    telefono: '72230940',
+    options: 'edit'
+  }]
+  export default {
+    components: {
+      LTable
+    },
+    data () {
+      return {
+        table1: {
+          columns: [...tableColumns],
+          data: [...tableData]
+        },
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
