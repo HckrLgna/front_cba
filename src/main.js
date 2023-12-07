f09425c3dfcd6e0df23c9f511cb7f77e9c89f354
Angel Eugenio Charca Blanco <ablanco837@gmail.com>
2023-12-07 16:57:32 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,35 +1,48 @@
+/*!
 
-import Vue from "vue";
-import VueRouter from "vue-router";
-import App from "./App.vue";
+ =========================================================
+ * Vue Light Bootstrap Dashboard - v2.1.0 (Bootstrap 4)
+ =========================================================
 
-// LightBootstrap plugin
-import LightBootstrap from "./light-bootstrap-main";
+ * Product Page: http://www.creative-tim.com/product/light-bootstrap-dashboard
+ * Copyright 2023 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard/blob/master/LICENSE.md)
 
-// router setup
-import routes from "./routes/routes";
+ =========================================================
 
-import "./registerServiceWorker";
-// plugin setup
-Vue.use(VueRouter);
-Vue.use(LightBootstrap);
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-// configure router
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-  linkActiveClass: "nav-item active",
-  scrollBehavior: (to) => {
-    if (to.hash) {
-      return { selector: to.hash };
-    } else {
-      return { x: 0, y: 0 };
-    }
-  },
-});
-/* eslint-disable no-new */
-new Vue({
-  el: "#app",
-  render: (h) => h(App),
-  router,
-});
+ */
+ import { createApp } from 'vue';
+ import { createRouter, createWebHistory } from 'vue-router';
+ import App from './App.vue';
+ import LightBootstrap from './light-bootstrap-main';
+ import routes from './routes/routes';
+
+ import './registerServiceWorker';
+
+ const app = createApp(App);
+
+ // Usa el enrutador VueRouter
+ const router = createRouter({
+   history: createWebHistory(),
+   base: '/admin',
+   routes,
+   linkActiveClass: 'nav-item active',
+   scrollBehavior: (to) => {
+     if (to.hash) {
+       return { selector: to.hash };
+     } else {
+       return { x: 0, y: 0 };
+     }
+   },
+ });
+
+ // Usa el plugin LightBootstrap
+ app.use(LightBootstrap);
+
+ // Usa el enrutador en tu aplicación
+ app.use(router);
+
+ // Monta la aplicación en el elemento con id 'app'
+ app.mount('#app');
